dd094967f06af55f89038509d2f5b1c88c48f460
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-01-15 00:21:58 +0100

    Feat: display food image and description

--- app/src/main/java/com/example/recipondo/MainActivity.kt
+++ app/src/main/java/com/example/recipondo/MainActivity.kt
@@ -3,36 +3,37 @@ package com.example.recipondo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.recipondo.ui.theme.RecipondoTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecipondoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Column {
+                Image(painterResource(
+                    R.drawable.pasta), "Banner Image",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Respondo Pasta")
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "600 Calories")
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "Italian Dish")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RecipondoTheme {
-        Greeting("Android")
-    }
-}
